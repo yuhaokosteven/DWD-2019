f871aad43c65dfbd331978dfed0da3033388fbf5
Yuhao Ko <31877110+yuhaokosteven@users.noreply.github.com>
2019-04-28 22:42:41 -0400

    Update partC.js

--- DWD_HW2/partC.js
+++ DWD_HW2/partC.js
@@ -1,35 +1,40 @@
+// HW 2c
+var mustacheExpress = require('mustache-express');
+
 var express = require('express')
+
+var request = require('request')
 
 var app = express()
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-
-var mustacheExpress = require('mustache-express');
-
-// Register '.mustache' extension with The Mustache Express
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname);
 
+
 app.get('/', function(req, res) {
-
-  if (req.query.url) {
-    var newURL = req.query.input_name
-
-    // res.send('Cool brand bro')
-    return res.redirect(newURL)
+  // console.log()
+  if (req.query.input_name) {
+    request(req.query.input_name, function(err, response, html) {
+      res.send(html)
+    })
+  } else {
+    res.render('url')
   }
-  // res.end()
-  res.render('url', {
-    // template_name: req.query.input_name
-  })
-
-
 })
+
+////get input example
+
+// app.get('/', function(req,res){
+//   res.render('inClassinput')
+// })
+
+
+// //mustache example
+//
+// app.get('/inClassinput', function(req,res){
+//
+// })
 
 app.listen(8000, function() {
   console.log('Web Server Started at port 8000')
